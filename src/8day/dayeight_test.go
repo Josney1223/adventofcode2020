2ab7c8a08ba4e9eb2008443a297374c64fb9ed75
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-10-08 16:41:13 -0300

    day 8 solved

--- src/8day/dayeight_test.go
+++ src/8day/dayeight_test.go
@@ -6,20 +6,39 @@ import (
 )
 
 func TestExampleOne(t *testing.T) {
-	result, err := InterpretCommandsOne("example.txt")
+	result, ty, err := InterpretCommandsOne("example.txt")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	if result != 5 {
+	if result != 5 && ty != 0 {
 		log.Fatalln("Resultado errado, esperado 5, recebido", result)
 	}
-	log.Print(result)
+	log.Print(ty, result)
+}
+
+func TestExampleTwo(t *testing.T) {
+	result, ty, err := FindReplaceCorruptedCommand("example.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if result != 8 && ty != 1 {
+		log.Fatalln("Resultado errado, esperado 8 1, recebido", result, ty)
+	}
+	log.Print(ty, result)
 }
 
 func TestCommandsOne(t *testing.T) {
-	result, err := InterpretCommandsOne("data.txt")
-	if err != nil {
+	result, ty, err := InterpretCommandsOne("data.txt")
+	if err != nil && ty != 0 {
 		log.Fatalln(err)
 	}
-	log.Print(result)
+	log.Print(ty, result)
+}
+
+func TestCommandsTwo(t *testing.T) {
+	result, ty, err := FindReplaceCorruptedCommand("data.txt")
+	if err != nil && ty != 0 {
+		log.Fatalln(err)
+	}
+	log.Print(ty, result)
 }
--- src/8day/main.go
+++ src/8day/main.go
@@ -2,15 +2,16 @@ package dayeight
 
 import (
 	"bufio"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func InterpretCommandsOne(path string) (int, error) {
+func InterpretCommandsOne(path string) (int, int, error) {
 	commands, err := OpenCommandsFile(path)
 	if err != nil {
-		return -1, err
+		return -1, -1, err
 	}
 
 	commandExecutedList := []bool{}
@@ -22,27 +23,82 @@ func InterpretCommandsOne(path string) (int, error) {
 
 		commandExecutedList = append(commandExecutedList, false)
 	}
-	return ExecuteCommands(commandsNew, commandExecutedList), nil
+	acc, t, _ := ExecuteCommands(commandsNew, commandExecutedList)
+	return acc, t, nil
 }
 
-func ExecuteCommands(commands [][]string, commandExecutedList []bool) int {
+func FindReplaceCorruptedCommand(path string) (int, int, error) {
+	commands, err := OpenCommandsFile(path)
+	if err != nil {
+		return -1, -1, err
+	}
+
+	commandExecutedListDefault := []bool{}
+	commandsNew := [][]string{}
+
+	for _, i := range commands {
+		splitString := strings.Split(i, " ")
+		commandsNew = append(commandsNew, splitString)
+
+		commandExecutedListDefault = append(commandExecutedListDefault, false)
+	}
+
+	commandExecutedList := []bool{}
+	commandExecutedList = append(commandExecutedList, commandExecutedListDefault...)
+
+	_, _, firstRun := ExecuteCommands(commandsNew, commandExecutedList)
+
+	for _, cmd := range firstRun {
+		commandExecutedList = []bool{}
+		commandExecutedList = append(commandExecutedList, commandExecutedListDefault...)
+		end := -1
+		acc := 0
+
+		if commandsNew[cmd][0] == "jmp" {
+			commandsNew[cmd][0] = "nop"
+			acc, end, _ = ExecuteCommands(commandsNew, commandExecutedList)
+			commandsNew[cmd][0] = "jmp"
+		} else if commandsNew[cmd][0] == "nop" {
+			commandsNew[cmd][0] = "jmp"
+			acc, end, _ = ExecuteCommands(commandsNew, commandExecutedList)
+			commandsNew[cmd][0] = "nop"
+		}
+
+		if end == 1 {
+			return acc, 1, nil
+		}
+	}
+
+	return 0, -1, nil
+}
+
+// Notação BigO: Performance (n), Memoria (n)
+//
+// Execute uma lista de commandos, pare quando chegar no primeiro espaço
+// vazio depois do fim da lista ou caso repita algum comando
+func ExecuteCommands(commands [][]string, commandExecutedList []bool) (int, int, []int) {
 	acc := 0
 	cursor := 0
+	steps := []int{}
 
-	for !commandExecutedList[cursor] {
+	for !commandExecutedList[cursor] && cursor != len(commands) {
+
 		command := commands[cursor]
 		commandExecutedList[cursor] = true
+		steps = append(steps, cursor)
 
 		switch command[0] {
 		case "nop":
 			cursor++
+
 		case "jmp":
 			jmpRunes := []rune(command[1])
 			operator := jmpRunes[0]
 
 			num, err := strconv.Atoi(string(jmpRunes[1:]))
 			if err != nil {
-				return -1
+				log.Println(err)
+				return -1, -1, steps
 			}
 
 			if operator == []rune("-")[0] {
@@ -52,16 +108,33 @@ func ExecuteCommands(commands [][]string, commandExecutedList []bool) int {
 			}
 
 		case "acc":
-			num, err := strconv.Atoi(command[1])
+			accRunes := []rune(command[1])
+			operator := accRunes[0]
+
+			num, err := strconv.Atoi(string(accRunes[1:]))
 			if err != nil {
-				return -1
+				log.Println(err)
+				return -2, -2, steps
 			}
-			acc += num
+
+			if operator == []rune("-")[0] {
+				acc -= num
+			} else {
+				acc += num
+			}
 			cursor++
 		}
 
+		if cursor >= len(commands) || cursor < 0 {
+			break
+		}
 	}
-	return acc
+
+	if cursor == len(commands) {
+		return acc, 1, steps
+	} else {
+		return acc, 0, steps
+	}
 }
 
 func OpenCommandsFile(path string) ([]string, error) {
