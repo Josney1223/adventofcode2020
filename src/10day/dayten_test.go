585d440bdc888298eb4e14086cba58b6a3e76f10
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-10-09 17:54:25 -0300

    day 10 progress challenge

--- src/10day/dayten_test.go
+++ src/10day/dayten_test.go
@@ -5,7 +5,40 @@ import (
 	"testing"
 )
 
+func TestExampleOnePartTwo(t *testing.T) {
+	result, err := CalculateCombinationsJolts("example.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if result != 8 {
+		log.Fatalln("Resultado errado, esperado 8, recebido", result)
+	}
+	log.Print(result)
+}
+
+func TestExampleTwoPartTwo(t *testing.T) {
+	result, err := CalculateCombinationsJolts("example1.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if result != 19208 {
+		log.Fatalln("Resultado errado, esperado 19208, recebido", result)
+	}
+	log.Print(result)
+}
+
 func TestExampleOne(t *testing.T) {
+	result, err := CalculateJoltsDiff("example.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if result != 35 {
+		log.Fatalln("Resultado errado, esperado 35, recebido", result)
+	}
+	log.Print(result)
+}
+
+func TestExampleTwo(t *testing.T) {
 	result, err := CalculateJoltsDiff("example1.txt")
 	if err != nil {
 		log.Fatalln(err)
--- src/10day/main.go
+++ src/10day/main.go
@@ -8,6 +8,59 @@ import (
 	"strconv"
 )
 
+func CalculateCombinationsJolts(path string) (int, error) {
+	jolts, err := OpenJoltsFile(path)
+	if err != nil {
+		return -1, err
+	}
+
+	jolts = append(jolts, 0)
+	sort.Ints(jolts)
+	jolts = append(jolts, jolts[len(jolts)-1]+3)
+
+	steps := []int{}
+
+	for i := range jolts {
+		if i+1 < len(jolts) {
+			if jolts[i+1]-jolts[i] == 1 {
+				steps = append(steps, 1)
+			} else if jolts[i+1]-jolts[i] == 2 {
+				steps = append(steps, 2)
+			} else if jolts[i+1]-jolts[i] == 3 {
+				steps = append(steps, 3)
+			}
+		}
+	}
+
+	combinations := 0
+	count := -1
+
+	for i := range steps {
+		if steps[i] == 1 {
+			count++
+		} else if steps[i] == 3 {
+			log.Println(combinations, count)
+			if count != 0 {
+				combinations = combinations * (2 * count)
+			}
+			count = -1
+		}
+	}
+
+	log.Println(steps)
+	log.Println(combinations)
+
+	return combinations, nil
+}
+
+func Fatorial(number int) int {
+	if number <= 1 {
+		return 1
+	} else {
+		return number * Fatorial(number-1)
+	}
+}
+
 func CalculateJoltsDiff(path string) (int, error) {
 	jolts, err := OpenJoltsFile(path)
 	if err != nil {
