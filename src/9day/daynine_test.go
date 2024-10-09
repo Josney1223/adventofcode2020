50e45b6cb2b5b87c9a953a87807d332ad4e72357
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-10-09 14:17:24 -0300

    second challange day 9 solved

--- src/9day/daynine_test.go
+++ src/9day/daynine_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestExampleOne(t *testing.T) {
-	result, err := CheckNumbersBasedPremeable("example.txt", 5)
+	result, _, err := CheckNumbersBasedPremeable("example.txt", 5)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -16,8 +16,27 @@ func TestExampleOne(t *testing.T) {
 	log.Print(result)
 }
 
+func TestExampleTwo(t *testing.T) {
+	result, err := FindEncryptionWeakness("example.txt", 5)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if result != 62 {
+		log.Fatalln("Resultado errado, esperado 62, recebido", result)
+	}
+	log.Print(result)
+}
+
 func TestFindWrongNumber(t *testing.T) {
-	result, err := CheckNumbersBasedPremeable("data.txt", 127)
+	result, _, err := CheckNumbersBasedPremeable("data.txt", 25)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Print(result)
+}
+
+func TestEncryptionWeakness(t *testing.T) {
+	result, err := FindEncryptionWeakness("data.txt", 25)
 	if err != nil {
 		log.Fatalln(err)
 	}
--- src/9day/main.go
+++ src/9day/main.go
@@ -7,9 +7,31 @@ import (
 	"strconv"
 )
 
-func CheckNumbersBasedPremeable(path string, start int) (int, error) {
+func CheckNumbersBasedPremeable(path string, start int) (int, int, error) {
 	numPreamble := start
 	wrongNumber := -1
+	index := -1
+
+	lines, err := OpenCypherFile(path)
+	if err != nil {
+		return -1, -1, err
+	}
+
+	for i := numPreamble; i < len(lines); i++ {
+		if !FindWrongNumber(lines[i], lines[i-numPreamble:i]) {
+			wrongNumber = lines[i]
+			index = i
+			break
+		}
+	}
+
+	return wrongNumber, index, nil
+}
+
+func FindEncryptionWeakness(path string, start int) (int, error) {
+	numPreamble := start
+	wrongNumber := -1
+	index := -1
 
 	lines, err := OpenCypherFile(path)
 	if err != nil {
@@ -17,17 +39,49 @@ func CheckNumbersBasedPremeable(path string, start int) (int, error) {
 	}
 
 	for i := numPreamble; i < len(lines); i++ {
-		if !CheckNumbers(lines[i], lines[i-numPreamble:i]) {
+		if !FindWrongNumber(lines[i], lines[i-numPreamble:i]) {
 			wrongNumber = lines[i]
+			index = i
 			break
 		}
 	}
 
-	return wrongNumber, nil
+	acc := 0
+	intervalMax := 0
+	intervalMin := 0
 
+out:
+	for j := 0; j < index; j++ {
+		acc = lines[j]
+		for n := j + 1; n < index; n++ {
+			acc += lines[n]
+			if acc == wrongNumber {
+				intervalMax = n + 1
+				intervalMin = j
+				break out
+			} else if acc > wrongNumber {
+				break
+			}
+		}
+	}
+
+	maxNumber := -1
+	minNumber := lines[intervalMin]
+	if intervalMax != -1 {
+		for _, i := range lines[intervalMin:intervalMax] {
+			if i < minNumber {
+				minNumber = i
+			} else if i > maxNumber {
+				maxNumber = i
+			}
+		}
+	}
+
+	return minNumber + maxNumber, nil
 }
 
-func CheckNumbers(number int, preamble []int) bool {
+// Notação BigO: Performance (n^2), Memoria (1)
+func FindWrongNumber(number int, preamble []int) bool {
 	found := false
 
 	for i := 0; i < len(preamble); i++ {
